14afe5306f05cbe019ecfe54bf537d6d67d69b8e
Dan Adler <daadler64@gmail.com>
2017-03-03 10:50:30 -0500

    Updates to allow interaction between fxml and Java program

--- MainProject/src/com/cypher/MessageEncrypt.java
+++ MainProject/src/com/cypher/MessageEncrypt.java
@@ -5,10 +5,18 @@ package com.cypher;
  */
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class MessageEncrypt extends Application {
 
@@ -18,10 +26,14 @@ public class MessageEncrypt extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent scene = FXMLLoader.load(getClass().getResource("MessageEncrypt.fxml"));
-
-        primaryStage.setTitle("Message Encryptors");
-        primaryStage.setScene(new Scene(scene, 600, 400));
-        primaryStage.show();
+        try {
+            SplitPane page = (SplitPane) FXMLLoader.load(getClass().getResource("MessageEncrypt.fxml"));
+            Scene scene = new Scene(page);
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Message Encryptor");
+            primaryStage.show();
+        } catch (Exception ex) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, "CRASH!!!", ex);
+        }
     }
 }
